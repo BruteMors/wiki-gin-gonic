23ffa7fd56ea395fafcde40a1da58b621158ac22
DVPDev <kors575@gmail.com>
2022-06-14 19:43:34 +0300

    Init message

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+
+	})
+
+	router.Run()
+}
